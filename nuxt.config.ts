11a0ceacd2fdcc466b740bda4a63f8b2796c37d2
Carina Meireles <carinacmeireles@gmail.com>
2024-01-21 22:30:14 +0000

    Deploy vercel proxy server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,19 +9,4 @@ export default {
       autoprefixer: {},
     },
   },
-  serverMiddleware: [
-    (_: any, res: ServerResponse, next: () => void) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-      );
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With, Content-Type, Authorization"
-      );
-
-      next();
-    },
-  ],
 };
